57e165eb03ad1e01a86e5b251231ab7cbdde8b1e
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-01-20 15:39:34 +0100

    adding ReplaceFirstTextCommand

---
+++ src/main/java/Oppgave1/ReplaceFirstTextCommand.java
@@ -0,0 +1,12 @@
+package Oppgave1;
+
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String execute(String text) {
+    return super.execute(text);
+  }
+}
