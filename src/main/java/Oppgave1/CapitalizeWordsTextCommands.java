034fa655c0c2ff199af3691d54eae33efd581482
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-01-20 16:03:59 +0100

    adding CapitalizeWordsTextCommands

---
+++ src/main/java/Oppgave1/CapitalizeWordsTextCommands.java
@@ -0,0 +1,15 @@
+package Oppgave1;
+
+public class CapitalizeWordsTextCommands extends CapitalizeTextCommand {
+  @Override
+  public String execute(String text) {
+    String[] words = text.split(" ");
+    StringBuilder sb = new StringBuilder();
+    for (String word : words) {
+      sb.append(word.substring(0, 1).toUpperCase());
+      sb.append(word.substring(1));
+      sb.append(" ");
+    }
+    return sb.toString().trim();
+  }
+}
